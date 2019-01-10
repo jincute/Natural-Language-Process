b477e61927b455df527b065cc26a0bf73286449b
Jinfang Luo <jincute@gmail.com>
2019-01-09 22:02:40 -0500

    Add files via upload

---
+++ analyze.py
@@ -0,0 +1,159 @@
+# To analyze the TP data
+# wujiechen@gmail.com
+# 2018-03-01
+#
+
+import nltk, re, pprint
+from nltk import word_tokenize
+import re
+import glob
+import pdb
+import json    # For print dictionary
+import sys
+
+
+# Input a line
+# Output (Forme,Lemma) pair
+def splitlemma(line):
+    # Split the form and lemma
+    form, lemma = line.split('\t')
+    # Remove the carriage return
+    lemma = lemma.strip()
+    return form, lemma
+
+
+def get_lemma_form_from_file(fname):
+    total_tokens = 0
+    lines = {}
+
+    # Meta info line
+    lines["meta"] = {}
+    # Non-alphabetical tokens
+    lines["non_alpha_form"] = {}
+    fl_pairs = []
+
+    with open(fname, encoding='latin') as file:
+        for index, line in enumerate(file):
+            # Filter out the lines of the begin|end document meta info
+            if bool(re.match("#(?:begin|end)\sdocument", line)):
+                lines["meta"][index] = line
+
+            # Start to count the tokens
+            total_tokens += 1
+
+            form, lemma = splitlemma(line)
+
+            fl_pairs += [(lemma, form)]
+
+    return fl_pairs
+
+
+def analyze_single_file(fname, fixflag=False):
+    total_tokens = 0
+    lines = {}
+
+    # Meta info line
+    lines["meta"] = {}
+    # Non-alphabetical tokens
+    lines["non_alpha_form"] = {}
+    # Unreadable lemmas (Non-utf8 lemma)
+    list_non_utf8 = []
+    # Form-Lemma pairs
+    fl_pairs = []
+
+    if fixflag:
+        filefix = open(fname + '-fix', "w")
+
+    with open(fname, encoding='latin') as file:
+        for index, line in enumerate(file):
+            # Read the file in latin and encode it again to use the readline but bypass the utf-8 error
+            line_bytes = line.encode('latin')
+            while True:
+                try:
+                    line = line_bytes.decode('utf-8')
+                    break
+                except UnicodeDecodeError as inst:
+                    # After we tackle the UnicodeDecodeError it should be all fine.
+                    old_char = line_bytes[inst.args[2]]
+                    line_bytes = line_bytes.replace(bytes([old_char]),
+                                                    bytes([old_char - (ord('a') - ord('A'))]))
+                    # Finish save!
+                    # Do the statistic
+                    list_non_utf8 += [(index, old_char)]
+
+            # Filter out the lines of the begin|end document meta info
+            if bool(re.match("#(?:begin|end)\sdocument", line)):
+                lines["meta"][index] = line
+                if fixflag:
+                    filefix.writelines(line)
+                continue
+
+            # Start to count the tokens
+            total_tokens += 1
+
+            form, lemma = splitlemma(line)
+
+            # lowercase lemma
+            lemma = lemma.lower()
+
+            if not form.isalpha() and lemma.isalpha():
+                lines["non_alpha_form"][index] = line
+
+            fl_pairs += [(lemma, form)]
+            if fixflag:
+                filefix.writelines(form + '\t' + lemma + '\n')
+
+    if fixflag:
+        filefix.close()
+
+    print("===")
+    print(fname)
+    # Show the lines statistics
+    print(list((x, len(lines[x])) for x in lines))
+
+    print("How many lines contain non-utf8:")
+    print(len(nltk.Index([(x, y) for x, y in list_non_utf8])))
+
+    print("How many kinds of first byte error non-utf8")
+    print(sorted(list(nltk.Index([(hex(y), x) for x, y in list_non_utf8]))))
+
+    print("---")
+    # Start Lemma statistics
+    print("Total Terms: {}".format(len(fl_pairs)))
+    idx = nltk.Index(fl_pairs)
+    list_lemma_freq = [(x, len(idx[x])) for x in idx]
+    list_lemma_freq.sort(key=lambda x: x[1], reverse=True)
+    topn = 20
+    print("Lemma frequncy Top{}:".format(topn))
+    print(list_lemma_freq[:topn])
+    print(sum([x[1] for x in list_lemma_freq[:topn]])/len(fl_pairs))    # Top20 40.5% Top100 53%
+
+    idx_set = nltk.Index(set(fl_pairs))
+    list_form_uni = [x for x in idx_set if len(idx[x]) == 1]
+    print("Lemma just has one form ratio:")  # About 50%
+    print(len(list_form_uni)/len(idx_set))
+
+    print("Lemma et form est identical:")
+    print(len([x for (x, y) in fl_pairs if x == y])/len(fl_pairs))  # About 68%
+
+    cfd = nltk.ConditionalFreqDist(fl_pairs)
+    print("Show lemma 'be' show in which form and how many times for each")
+    print(json.dumps(cfd['be'], ensure_ascii=False))
+
+    list_lemma_forme_freq = [(x, len(cfd[x])) for index, x in enumerate(cfd)]
+    list_lemma_forme_freq.sort(key=lambda x: x[1], reverse=True)
+
+    return fl_pairs
+    # pdb.set_trace()
+
+
+if len(sys.argv) > 1:
+    # Define the data path
+    data_path_dev = sys.argv[1]
+
+if __name__ == '__main__':
+    # File name should be defined here, otherwise it tackle all the files in the folder
+    path = data_path_dev + 'dev-24'
+    list_of_file = sorted(glob.glob(path))
+    for file in list_of_file:
+        analyze_single_file(file)
---
+++ try_models.py
@@ -0,0 +1,174 @@
+# Try models
+# wujiechen@gmail.com
+# 2018-03-01
+#
+
+import analyze
+import glob
+import nltk
+import time
+from collections import deque
+from copy import copy
+import pdb
+from datetime import datetime
+import ujson
+
+
+# Calculate the accuracy by giving the classes result list.
+def cal_accuracy(class_r, test_set):
+    correct = 0
+    for index, c in enumerate(class_r):
+        if c == test_set[index][1]:
+            correct += 1
+    return correct/len(class_r)
+
+
+# Naive Bayes ngram
+def try_naive_bayes_ngram(train_set_in, test_set, ngram=2):
+    print("len(train_set)= {}, len(test_set)= {}".format(len(train_set_in), len(test_set)))
+    train_set = copy(train_set_in)
+    # Dictionary for all the features of each lemma we have seen.
+    dict_features_set = {}
+    # Dictionary for all the classifier of each lemma we have seen.
+    dict_classifier = {}
+
+    # extract feature
+    bigrams_lemma = list(nltk.bigrams([l for l, f in train_set]))
+    bigrams_lemma = [('', train_set[0][0])]+bigrams_lemma
+
+    ngrams_lemma = list(nltk.ngrams([l for l, f in train_set],ngram))
+    complete_pair = []
+    for i in range(1, ngram):
+        complete_pair.append(tuple([''] * (ngram - i) + [lf[0] for lf in train_set[:i]]))
+    ngrams_lemma = complete_pair + ngrams_lemma
+
+    labeled_bigrams_train_set = list(zip(ngrams_lemma, [f for l, f in train_set]))
+    train_lemma_cfd = nltk.ConditionalFreqDist(train_set)
+    train_inv = nltk.ConditionalFreqDist([(l, p) for p, l in labeled_bigrams_train_set])
+
+    # find and add lemma_{j-1},lemma_j for all form_i correspond to each lemma_i
+    for lemma, listf in train_lemma_cfd.items():
+        if lemma not in dict_features_set:
+            dict_features_set[lemma] = []
+        for f in listf:
+            for pair in train_inv[f]:
+                if MULTI_VALUE_FEATURE:
+                    dict_features_set[lemma] += [({"lemma"+str(i): p for i, p in enumerate(pair)}, f)] * train_inv[f][pair]
+                else:
+                    dict_features_set[lemma] += [({p: True for p in pair[:ngram]}, f)]*train_inv[f][pair]
+    print("Start training... N= ", len(dict_features_set))
+    print("Time:", str(datetime.now()))
+    # Train for each lemma in the feature set
+    for index, lemma in enumerate(dict_features_set):
+        if index % 10000 == 0:
+            print("Index:", index)
+            print("Time:", str(datetime.now()))
+        train_set_w = dict_features_set[lemma]
+        if lemma not in dict_classifier:
+            dict_classifier[lemma] = nltk.NaiveBayesClassifier.train(train_set_w)
+    list_result = []
+    print("Test data...")
+    print("Time:", str(datetime.now()))
+    # Test the data, do the prediction
+    gram_wnd = deque([""] * ngram, maxlen=ngram)
+    gram_wnd.extend(test_set[:ngram - 1])
+    for index, (lemma, form) in enumerate(test_set):
+        if index % 10000 == 0:
+            print("Index:", index)
+            print("Time:", str(datetime.now()))
+        gram_wnd.append(lemma)
+        if lemma in dict_classifier:
+            if MULTI_VALUE_FEATURE:
+                list_result += [dict_classifier[lemma].classify({"lemma"+str(i): p for i, p in enumerate(gram_wnd)})]
+            else:
+                list_result += [dict_classifier[lemma].classify({gram_wnd[i]: True for i, x in enumerate(gram_wnd)})]
+        else:
+            list_result += [lemma]
+    print("Classifier accuracy percent (Naive Bayes N-gram: n={}): {}".format(ngram, cal_accuracy(list_result,
+                                                                                                test_set) * 100))
+    ujson.dump(list_result, open('/Users/jason.wu/Downloads/list_result_ngram.json', 'w'))
+    ujson.dump(test_set, open('/Users/jason.wu/Downloads/test_set_ngram.json', 'w'))
+    pdb.set_trace()
+
+
+# Simple MLE
+def try_simple_max_freq(train_set, test_set):
+    cfd = nltk.ConditionalFreqDist(train_set)
+    pred_class = []
+    for p in test_set:
+        if p[0] in cfd:
+            pred_class += [cfd[p[0]].max()]
+        else:
+            # If it exist pas, we use the lemme as the form
+            pred_class += [p[0]]
+    print("Classifier accuracy percent (Simple max Freq):", cal_accuracy(pred_class, test_set) * 100)  # About 78%
+
+
+# Ngram to predict wether the lemma equals to form
+def try_leqf_ngram(train_set, test_set, ngram=2):
+    gram_wnd = deque([""] * ngram, maxlen=ngram)
+    gram_wnd.extend(train_set[:ngram - 1])
+    train_set_f = []
+    for (lemma, forme) in train_set[ngram-1:]:
+        gram_wnd.append(lemma)
+        train_set_f += [({"lemma": lemma, "ngram": tuple(gram_wnd)}, lemma == forme)]
+    _classifier = nltk.NaiveBayesClassifier.train(train_set_f)
+    gram_wnd = deque([""] * ngram, maxlen=ngram)
+    gram_wnd.extend(test_set[:ngram - 1])
+    test_set_f = []
+    for (lemma, forme) in test_set[ngram-1:]:
+        gram_wnd.append(lemma)
+        test_set_f += [({"lemma": lemma, "ngram": tuple(gram_wnd)}, lemma == forme)]
+    print("Classifier accuracy percent Ngram (n={}) lemma = form : {}".format(
+        ngram, nltk.classify.accuracy(_classifier, test_set_f)*100))
+    _classifier.show_most_informative_features(5)
+
+
+# Split train and test data for the given raio
+def split_train_test(_features_set, ratio=0.9):
+    _train_set = _features_set[:int(len(_features_set) * ratio)]
+    _test_set = _features_set[len(_train_set):]
+    return _train_set, _test_set
+
+
+def demo():
+    global list_of_file, fl_pairs
+    for file in list_of_file:
+        fl_pairs += analyze.get_lemma_form_from_file(file)
+
+    start = time.time()
+
+    features_set = fl_pairs
+    train_set, test_set = split_train_test(features_set)
+
+    # Start predict lemma = equals
+    #try_leqf_ngram(train_set, test_set, 1)
+    # Ngram to predict wether the lemma equals to form n=2
+    #try_leqf_ngram(train_set, test_set, 2)
+    # Ngram to predict wether the lemma equals to form n=3
+    #try_leqf_ngram(train_set, test_set, 3)
+    # End predict lemma = equals
+
+    print("---")
+    print("Start Time: ", str(datetime.now()))
+
+    # Simple max Freq Unigram
+    #try_simple_max_freq(train_set, test_set)
+
+    # Naive Bayes ngram n=2
+    try_naive_bayes_ngram(train_set, test_set, 2)
+
+    end = time.time()
+    print("End Time:", str(datetime.now()))
+
+
+# File name should be defined here, otherwise it tackle all the files in the folder
+path = analyze.data_path_dev + 'dev-24'
+list_of_file = sorted(glob.glob(path))
+fl_pairs = []
+MULTI_VALUE_FEATURE = True
+
+if __name__ == '__main__':
+    demo()
+
+pdb.set_trace()
